c6ff8e745ec666646e4fc688a27df9f6158f3879
MARK PEARYER <markpearyer@MARKs-Air.attlocal.net>
2015-06-22 20:50:46 -0700

    Mark Scaffolding Commit

---
+++ app/scripts/scripts.js
@@ -0,0 +1,17 @@
+/* For picking letters from alphabet keypad. Returns the
+   letter chosen or "already picked"
+*/
+function pickLetter(letter) {
+  "use strict";
+  var result;
+
+  if (letter.className === "letter-button") {
+    result = letter.innerHTML;
+    letter.className = "letter-disabled";
+  } else {
+    result = "already picked";
+  }
+
+  console.log(result);    // for testing, remove later
+  return result;
+}
