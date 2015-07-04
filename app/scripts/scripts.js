7c3a51b2123e32335e4f188bef7e8ceee08272f7
Simon <simonxca@yahoo.ca>
2015-07-03 21:00:59 -0400

    removed extra logic from scripts.js per Cory's comments

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,31 +1,20 @@
-(function () {
-  "use strict";
+// add pickLetter to all buttons on alphabet keypad
+$(".letter-button").on("click", pickLetter);
   
-  // add pickLetter to all buttons on alphabet keypad
-  $(".letter-button").on("click", pickLetter);
-  
-  // pick a letter from alphabet keypad
-  function pickLetter() {
-    var button = $(this),
-        letterChosen;
-        
-    if (button.attr("class") === "letter-button") {
-      letterChosen = button.html();
-      disableButton(button);
-      
-    } else {
-      letterChosen = "already picked";
-    }
-	
-    verify(letterChosen);
-	}
-  
-  function disableButton(button) {
-    button.removeClass("letter-button").addClass("letter-disabled");
-  }
-}());
+// pick a letter from alphabet keypad
+function pickLetter() {
+    var currentLetter = $(this);
+
+    currentLetter.removeClass("letter-button").addClass("letter-disabled");
+
+    // Next two lines to be refactored to the interface for checking if letters are in the word.
+
+    currentLetter = currentLetter.html();
+
+    verify(currentLetter);
+}
 
 // currently a placeholder for verifying the letter
 function verify(letter) {
-  console.log(letter);
+    console.log(letter);
 }
