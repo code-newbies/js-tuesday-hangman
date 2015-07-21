2a4cc3ffe1207cb95b9e7a8233ca8c9b11788937
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-07-21 07:36:24 +0530

    adds a new body part for each wrong guess made by a user

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,55 @@
 /*
  * Pick from alphabet keypad. Returns the letter chosen.
  */
-$("#alphabet-keypad").on("click", ".letter-button", pickLetter);
 
-function pickLetter() {
-  var letterChosen = $(this);
-  
-  letterChosen
-    .removeClass("letter-button")
-    .addClass("letter-disabled");
+function pickLetter(letter) {
+  "use strict";
+  var result;
+  //checks if the class name is still a button or not
+  if (letter.className === "letter-button") {
+    //if it's still a button it returns the letter
+    //then disables the button
+    result = letter.innerHTML;
+    letter.className = "letter-disabled";
 
-  // Next two lines to be refactored into interface for
-  //   checking if the letter is in the word.
-  
-  letterChosen = letterChosen.html();
-  
-  verify(letterChosen);
+    //calls this function to deal with where the letter goes
+    handlePickedLetter(result);
+  } else {
+    //if the buttons is disabled it logs this
+    result = "already picked";
+  }
+  // for testing, remove later
+  console.log(result);
+}
+
+function handlePickedLetter(result) {
+  var resultMatches = [];
+  var ind = currentWord.indexOf(result.toLowerCase());
+
+  //if result matches one or more letters in the current word
+  //push all instances of that letter to resultMatches
+  while (ind !== -1) {
+    resultMatches.push(ind);
+    ind = currentWord.indexOf(result.toLowerCase(), ind + 1);
+  }
+
+  //if resultMatches is greater than 0 proceed to place them in the dom
+  if(resultMatches.length > 0) {
+    var letterBlocks = document.getElementsByClassName("is-letter");
+    resultMatches.map(function(num) {
+
+      var domElem = document.createElement("span");
+      domElem.innerHTML = currentWordFull[num];
+      letterBlocks[num].appendChild(domElem);
+    });
+  } else {
+    //if letterBlock is not greater than 0 put the letter in the graveyard
+    var domElem = document.createElement("div");
+    domElem.className = "grave-letter";
+    domElem.innerHTML = result;
+    document.getElementById("letter-graveyard").appendChild(domElem);
+    hangmanGraphic.addBodyPart();
+  }
 }
 
 // placeholder for verifying the letter
@@ -30,7 +64,7 @@ function verify(letter) {
 var hangmanGraphic = function () {
   var bodyParts = 0,
       maxParts = 7;
-  
+
   return {
     addBodyPart : function () {
       if (bodyParts < maxParts) {
@@ -38,7 +72,7 @@ var hangmanGraphic = function () {
         $("#hangman-frame" + bodyParts).css("opacity", 1);
       }
     },
-		
+
     reset : function () {
       $(".hangman-frames").css("opacity", 0);
       $("#hangman-frame0").css("opacity", 1);
@@ -81,3 +115,27 @@ function wordSelect (array) {
   var word = array[num];
   return word;
 }
+
+var currentWordFull = easyArray[47],//IMPORTANT: replace the number with wordSelect (the function) for production use
+//set an all lower case version of the current word
+currentWord = currentWordFull.toLowerCase();
+
+//creates blocks in the DOM indicating where there are letters and spaces
+currentWord.split("").map(function(elem) {
+  var guessWordBlock = document.getElementById("word-to-guess");
+
+  // var guessWordBlock = $("#word-to-guess")[0];
+
+  var domElem = document.createElement("div");
+
+
+  if(elem.match(/[a-z]/i)) {
+    domElem.className = "character-block is-letter";
+
+  } else {
+    domElem.className = "character-block";
+
+  }
+
+  guessWordBlock.appendChild(domElem);
+});
